121f9ece7ab9278cfde93b349cdf579eb84d30b0
ben <biniamargaw79@gmail.com>
2026-02-21 21:35:02 -0500

    added some comments

--- Vector3.cpp
+++ Vector3.cpp
@@ -1,20 +1,25 @@
+//This class contains 3d vector calculations. 
 #include "Vector3.h"
 #include <cmath>
+//this initializes the constructor variables 
 Vector3::Vector3(double x, double y, double z){
 	x_val = x;
 	y_val = y;
 	z_val = z; 
 }
+//this function takes in a vector and adds the vector with the current vector. It returns the sum of the two vectors 
 Vector3 Vector3::add(const Vector3 &other) const{
 	return Vector3(x_val + other.x_val,
 			y_val + other.y_val, 
 			z_val + other.z_val);
 }
+//this function returns a vector multiplied by a double scalar  
 Vector3 Vector3::multiply(double scalar) const {
 	return Vector3(x_val * scalar,
 			y_val * scalar, 
 			z_val * scalar);
 }
+//This function computes the distance and returns the double value 
 double Vector3::magnitude() const {
 	return std::sqrt((x_val * x_val) + (y_val * y_val) + (z_val * z_val)); 
 
--- physics.cpp
+++ physics.cpp
@@ -1,6 +1,7 @@
 #include "Physics.h"
 #include "Constants.h"
 #include <cmath>
+//This function takes in a vector position and computes the gravitational acceleration and returns the vector value 
 Vector3 Physics::computeGravity(const Vector3 &position){
 	double r  = position.magnitude(); 
 	double dist = r * r * r;
