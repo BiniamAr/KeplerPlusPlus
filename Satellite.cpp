dd2b292479fda8295d431559b112f394ced25c8d
ben <biniamargaw79@gmail.com>
2026-03-11 16:39:10 -0400

    Added comments to the satellite file

--- Satellite.cpp
+++ Satellite.cpp
@@ -1,3 +1,3 @@
 #include "Satellite.h"
-
+//This constructor takes in velocity and pos as a param and intilize them. 
 Satellite::Satellite(const Vector3 &vel, const Vector3 &pos) : Velocity(vel), Position(pos){}
